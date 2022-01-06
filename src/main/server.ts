df1b282e829de5fc86de4cc4f0b6b8585ef0d2d0
(jus)saras2 <aras.strong8@gmail.com>
2022-01-06 06:42:20 -0300

    refactor: server.ts

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,13 +1,13 @@
 
-// import { MongoHelper } from '../infra/db/mongodb/account-repository/helpers/mongo-helper'
+import { MongoHelper } from '../infra/db/mongodb/account-repository/helpers/mongo-helper'
 import env from './config/env'
-import app from './config/app'
-/* MongoHelper.connect('mongodb://localhost:27017/mydb')
+// import app from './config/app'
+MongoHelper.connect('mongodb://localhost:27017/mydb')
   .then(async () => {
     const app = (await import('./config/app')).default
 
     app.listen(env.PORT, () => console.log(`Server running at http://localhost:${env.PORT}`))
   })
   .catch((err) => console.error(err))
-*/
-app.listen(env.PORT, () => console.log(`Server running at http://localhost:${env.PORT}`))
+
+// app.listen(env.PORT, () => console.log(`Server running at http://localhost:${env.PORT}`))
