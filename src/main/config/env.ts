948d51d4295379c6fe5a8f25a58ac11497a849e2
(jus)saras2 <aras.strong8@gmail.com>
2022-01-06 11:24:00 -0300

    feat: dockerfile

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/mydb',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/mydb',
   PORT: process.env.PORT ?? 3000,
   JWT: process.env.JWT ?? 'secret'
 }
